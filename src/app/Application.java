7b9bfc556e1d56b2f64250102331f12edb85a6fe
Cardoso <dylan.cardoso@studentfr.ch>
2025-02-04 13:59:22 +0100

    first commit

--- src/app/Application.java
+++ src/app/Application.java
@@ -1,17 +1,40 @@
 package app;
 
 public class Application {
-    
-        //
-        //              )        (                 ) (           (         (
-        //           ( /(   *   ))\ )        (  ( /( )\ )        )\ )  (   )\ )
-        //     (   ( )\())` )  /(()/((       )\ )\()|()/(  (    (()/(  )\ (()/(
-        //     )\  )((_)\  ( )(_))(_))\    (((_|(_)\ /(_)) )\    /(_)|((_) /(_))
-        //    ((_)((_)((_)(_(_()|_))((_)   )\___ ((_|_))_ ((_)  (_)) )\___(_))
-        //    __   _____ _____ ___ ___    ___ ___  ___  ___   ___ ___ ___   _
-        //    \ \ / / _ \_   _| _ \ __|  / __/ _ \|   \| __| |_ _/ __|_ _| | |
-        //     \ V / (_) || | |   / _|  | (_| (_) | |) | _|   | | (__ | |  |_|
-        //      \_/ \___/ |_| |_|_\___|  \___\___/|___/|___| |___\___|___| (_)
-        //
+    public static void main(String[] args) {
 
+
+        Ami ami1 = new Ami("Dylan", 7, false);
+        Ami ami2 = new Ami("Tomas", 6, false);
+        Ami ami3 = new Ami("Elise", 5, false);   
+
+        while (true) {
+            if (ami1.getNbBiereAvantDetreSaoul() == 0 && !ami1.getEstCuit()) {
+                ami1.setEstCuit(true);
+                System.out.println(
+                ami1.getNom() + " ne peut plus boire de biere... les autres continue !");
+            } else if (ami2.getNbBiereAvantDetreSaoul() == 0 && !ami2.getEstCuit()) {
+                ami2.setEstCuit(true);
+                System.out.println(
+                ami2.getNom() + " ne peut plus boire de biere... les autres continue !");
+            } else if (ami3.getNbBiereAvantDetreSaoul() == 0 && !ami3.getEstCuit()) {
+                ami3.setEstCuit(true);
+                System.out.println(
+                ami3.getNom() + " ne peut plus boire de biere... les autres continue !");
+            }
+            if (ami1.getNbBiereAvantDetreSaoul() != 0) {
+                ami1.boitUneBiere();
+            }
+            if (ami2.getNbBiereAvantDetreSaoul() != 0) {
+                ami2.boitUneBiere();
+            }
+            if (ami3.getNbBiereAvantDetreSaoul() != 0) {
+                ami3.boitUneBiere();
+            }
+            if (ami1.getEstCuit() == true && ami2.getEstCuit() && ami3.getEstCuit() ) {
+                break;
+            }
+        }
+        System.out.println("mais enfaite tout le monde est Saouls...");
+    }
 }
